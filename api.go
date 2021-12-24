6b1b622942ff2e171ddcda6be9ae80b7431fad5d
James Yeap <jamesyeap.ys@gmail.com>
2021-12-24 16:47:23 +0800

    Another attempt at fixing CORS issue.

--- api.go
+++ api.go
@@ -40,25 +40,25 @@ type UpdateTaskParams struct {
 }
 
 // CORS middleware
-// func CORSMiddleware() gin.HandlerFunc {
-//     return func(c *gin.Context) {
-//         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-//         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-//         c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-//         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
-//         if c.Request.Method == "OPTIONS" {
-//             c.AbortWithStatus(204)
-//             return
-//         }
+        if c.Request.Method == "OPTIONS" {
+            c.AbortWithStatus(204)
+            return
+        }
 
-//         c.Next()
-//     }
-// }
+        c.Next()
+    }
+}
 
 func main() {
 	r := gin.Default()
-	// r.Use(CORSMiddleware());
+	r.Use(CORSMiddleware());
 	config := cors.DefaultConfig()
 	config.AllowMethods = []string{"GET", "POST"}
 	config.AllowAllOrigins = true
@@ -161,19 +161,6 @@ func main() {
 		// fmt.Println(params)
 		addTask(params)		
 	})
-
-	// allow CORS
-	// r.Use(cors.New(cors.Config{
-	// 	 AllowOrigins:     []string{"http://localhost:3000"},
-	// 	 AllowMethods:     []string{"GET", "PUT", "PATCH"},
-	// 	 AllowHeaders:     []string{"Origin"},
-	// 	 ExposeHeaders:    []string{"Content-Length"},
-	// 	 AllowCredentials: true,
-	// 	 AllowOriginFunc: func(origin string) bool {
-	// 	 	return origin == "https://github.com"
-	// 	 },
-	// 	 MaxAge: 12 * time.Hour,
-	// }))
 
 	// start the server
 	r.Run()
